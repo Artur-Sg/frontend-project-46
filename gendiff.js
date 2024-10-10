34c52c10a24061a180bb510dc8eb4d6b3253be70
Artur-Sg <aasgibnev@gmail.com>
2024-10-11 00:34:58 +0400

    feat(app): second step: add arguments and format option

--- gendiff.js
+++ gendiff.js
@@ -2,12 +2,16 @@
 
 import { Command } from 'commander';
 
-
 const program = new Command();
 
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0');
+  .version('1.0.0')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format')
+  .action((f1, f2, { format }) => {
+    console.log('f1', f1, 'f2', f2, 'format', format);
+  });
 
 program.parse();
